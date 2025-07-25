660eef934848e1ba41c407deac5769a2280a7d15
tomasoignons <emmanuel.b.omont@gmail.com>
2025-07-25 15:25:44 +0200

    corrected url

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,6 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0', // Allow access from all network interfaces
-    allowedHosts: ['publibike.omont.ch'], // Add the allowed host here
+    allowedHosts: ['statslife.omont.ch'], // Add the allowed host here
   },
 })
